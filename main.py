2f2cca749aa90d4ea68319b43430e1d4c8383151
Bobiinette <nic1212c@gmail.com>
2025-03-29 15:40:47 +0100

    Added report structure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from stats import get_number_of_words
 from stats import get_number_of_each_characters
+from stats import sort_character_dictionnary
 
 def get_book_text(file_path):
     file_content = ""
@@ -8,10 +9,18 @@ def get_book_text(file_path):
     return file_content
 
 def main():
+    print("============ BOOKBOT ============")
     books = ["./books/frankenstein.txt"]
     for book in books:
-        print(f"{get_number_of_words(get_book_text(book))} words found in the document")
-        print(get_number_of_each_characters(get_book_text(book)))
+        print(f"Analyzing book found at {book[2:]}...")
+        print("----------- Word Count ----------")
+        print(f"Found {get_number_of_words(get_book_text(book))} total words")
+        print("--------- Character Count -------")
+        characters_sorted = sort_character_dictionnary(get_number_of_each_characters(get_book_text(book)))
+        for c in characters_sorted:
+            print(f"{c["character"]}: {c["count"]}")
+        print()
+    print("============= END ===============")
     return 0
 
 main()
