92e27fcd260c86e1bb313bfb6190d31b54bc4639
Bobiinette <nic1212c@gmail.com>
2025-03-29 15:08:25 +0100

    Refactoring get get_number_of_words in a stats.py

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
+from stats import get_number_of_words
+
 def get_book_text(file_path):
     file_content = ""
     with open(file_path) as f:
         file_content = f.read()
     return file_content
-
-def get_number_of_words(book_content):
-    return len(book_content.split())
 
 def main():
     books = ["./books/frankenstein.txt"]
---
+++ stats.py
@@ -0,0 +1,2 @@
+def get_number_of_words(book_content):
+    return len(book_content.split())
