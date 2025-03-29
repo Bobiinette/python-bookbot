71710dae2f253a7241fefeba0d67aff71dc1d60e
Bobiinette <nic1212c@gmail.com>
2025-03-29 15:03:50 +0100

    Docker compose for simplicity

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-print("greetings boots")
+def get_book_text(file_path):
+    file_content = ""
+    with open(file_path) as f:
+        file_content = f.read()
+    return file_content
+
+def get_number_of_words(book_content):
+    return len(book_content.split())
+
+def main():
+    books = ["./books/frankenstein.txt"]
+    for book in books:
+        print(f"{get_number_of_words(get_book_text(book))} words found in the document")
+    return 0
+
+main()
